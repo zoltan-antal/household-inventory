31816acf7a23e2b5bfe2a01c65165ad7a17426e7
zolantal <zoltan_antal@outlook.com>
2024-06-11 17:10:17 +0100

    Add product information fetching using barcode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,50 @@
+import { useEffect, useState } from 'react';
 import BarcodeScanner from './BarcodeScanner';
+import axios from 'axios';
+
+interface ProductData {
+  product: {
+    product_name: string;
+    brands: string;
+  };
+}
 
 function App() {
+  const BARCODE_BASE_URL = 'https://world.openfoodfacts.org/api/v2/product/';
+  const [barcode, setBarcode] = useState('');
+  const [product, setProduct] = useState('');
+  const [brand, setBrand] = useState('');
+
+  useEffect(() => {
+    const fetchBarcodeData = async () => {
+      if (!barcode) {
+        return;
+      }
+
+      try {
+        const response: { data: ProductData } = await axios.get(
+          `${BARCODE_BASE_URL}${barcode}`
+        );
+        const productName = response.data.product.product_name;
+        const brandName = response.data.product.brands;
+        setProduct(productName || 'error');
+        setBrand(brandName || 'error');
+      } catch (error) {
+        setProduct('error');
+        setBrand('error');
+      }
+    };
+
+    fetchBarcodeData();
+  }, [barcode]);
+
   return (
     <>
       <h1>Household Inventory</h1>
-      <BarcodeScanner></BarcodeScanner>
+      <BarcodeScanner setBarcode={setBarcode}></BarcodeScanner>
+      <p>Barcode: {barcode}</p>
+      <p>Product: {product === 'error' ? 'Not found' : product}</p>
+      <p>Brand: {brand === 'error' ? 'Not found' : brand}</p>
     </>
   );
 }
--- frontend/src/BarcodeScanner.tsx
+++ frontend/src/BarcodeScanner.tsx
@@ -1,21 +1,19 @@
-import { useState } from 'react';
 import { useZxing } from 'react-zxing';
 
-const BarcodeScanner = () => {
-  const [result, setResult] = useState('');
+interface BarcodeScannerProps {
+  setBarcode: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const BarcodeScanner = ({ setBarcode }: BarcodeScannerProps) => {
   const { ref } = useZxing({
     onDecodeResult(result) {
-      setResult(result.getText());
+      setBarcode(result.getText());
     },
   });
 
   return (
     <>
       <video ref={ref} />
-      <p>
-        <span>Result:</span>
-        <span>{result}</span>
-      </p>
     </>
   );
 };
